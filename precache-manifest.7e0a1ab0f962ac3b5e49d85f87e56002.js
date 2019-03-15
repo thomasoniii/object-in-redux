3cb565229d9f882a28026406215622a2770e4d4d
Jim Thomason <jim@japanesewartuba.com>
2019-03-15 15:33:10 +0000

    Updates

---
+++ precache-manifest.7e0a1ab0f962ac3b5e49d85f87e56002.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e23258ac95aa6dd3e7d",
+    "url": "/object-in-redux/static/js/runtime~main.541ed90c.js"
+  },
+  {
+    "revision": "36c9ff398f335075ec6d",
+    "url": "/object-in-redux/static/js/main.0d102f87.chunk.js"
+  },
+  {
+    "revision": "557da1ec4db7262fa620",
+    "url": "/object-in-redux/static/js/2.3b18b3aa.chunk.js"
+  },
+  {
+    "revision": "36c9ff398f335075ec6d",
+    "url": "/object-in-redux/static/css/main.e8e89b01.chunk.css"
+  },
+  {
+    "revision": "2b10905ce01bc673722281cf289a6763",
+    "url": "/object-in-redux/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/object-in-redux/precache-manifest.7e0a1ab0f962ac3b5e49d85f87e56002.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/object-in-redux/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
